f8c2409dec013af74db66ffbbe685b2bc63aaba1
tmps880305 <tmps880305@gmail.com>
2023-04-01 17:48:15 -0400

    First file commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/main-style.css"/>
+    <link rel="stylesheet" type="text/css" href="assets/css/main-page.css"/>
+    <link rel="stylesheet" type="text/css" href="assets/css/indi-navbar.css"/>
+    <link rel="stylesheet" type="text/css" href="assets/css/indi-footer.css"/>
+
+
+    <title>MasterMinds</title>
+</head>
+<body>
+<div class="main-background">
+    <div class="nav_container nav-fix">
+        <div class="navbar">
+            <div class="logo_container">
+                <a href="./index.html"><img src="assets/img/mastermind_logo_3.png" alt="logo"></a>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="./reference.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="./transport.html">Program Information</a>
+<!--                        <ul class="dropdown-content">-->
+<!--                            <li><a class="dropItem" href="./transport-between.html">All-stars</a></li>-->
+<!--                            <li><a class="dropItem" href="./transport-within.html">Events</a></li>-->
+<!--                            <li><a class="dropItem" href="./transport-within.html">Alumni Events</a></li>-->
+<!--                        </ul>-->
+                    </li>
+                    <li>
+                        <a href="./food.html">General Information</a>
+                    </li>
+                    <li>
+                        <a href="./drink.html">Leagues Information</a>
+<!--                        <ul class="dropdown-content">-->
+<!--                            <li><a class="dropItem" href="./transport-between.html">Results</a></li>-->
+<!--                        </ul>-->
+                    </li>
+                    <li>
+                        <a href="./reference.html">Teams</a>
+                    </li>
+                    <li id="nav-divider">
+                        <a href="./transport.html">Contact</a>
+                    </li>
+                    <li>
+                        <a href="./food.html">Relate Links</a>
+                    </li>
+                    <li>
+                        <a href="./drink.html">Store</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+
+</div>
+
+
+
+<footer class="footer">
+    <div>
+        <a>Thank you !</a>
+    </div>
+</footer>
+
+</body>
+</html>
